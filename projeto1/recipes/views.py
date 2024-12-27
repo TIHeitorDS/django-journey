fc5013f6c98b5cee8a4f66b700e4f92afb9806fc
TIHeitorDS <heitorclaudinods@gmail.com>
2024-12-27 14:13:21 -0300

    finalizando a base do projeto 1

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Heitor Claudino Desenvolvedor'
+        'name': 'Heitor Claudino'
     })
